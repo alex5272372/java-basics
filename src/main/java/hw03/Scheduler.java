80d951f5081cb126db5d1247502fb1a42422081d
o_nikolaenko <o.nikolaenko@astoundcommerce.com>
2019-10-15 17:01:32 +0300

    HW3 begin

---
+++ src/main/java/hw03/Scheduler.java
@@ -0,0 +1,21 @@
+package hw03;
+
+public class Scheduler {
+    public static void main(String[] args) {
+        String[][] scedule = new String[7][2];
+        scedule[0][0] = "sunday";
+        scedule[0][1] = "do home work";
+        scedule[1][0] = "monday";
+        scedule[1][1] = "go to courses; watch a film";
+        scedule[2][0] = "tuesday";
+        scedule[2][1] = "go to work office";
+        scedule[3][0] = "wednesday";
+        scedule[3][1] = "shopping";
+        scedule[4][0] = "thursday";
+        scedule[4][1] = "sleeping; watch TV";
+        scedule[5][0] = "friday";
+        scedule[5][1] = "cooking; eat";
+        scedule[6][0] = "saturday";
+        scedule[6][1] = "go to the gym";
+    }
+}
